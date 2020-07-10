492133b22d4c3a391832b701b621ae1261d71630
Le Huu Vo Long <long.leviathan14@hcmut.edu.vn>
2020-07-10 21:34:40 +0700

    Update to manager dashboard. Add member manager low-level functions like get, remove, list, update

--- controllers/manager.js
+++ controllers/manager.js
@@ -7,7 +7,43 @@ var utility = require('./utility');
 function isManagerLoggingIn(req) {
     return req.session.type == 'manager';
 }
+
+function getAllUserByType(type)
+{
+    return model.getUsersByType(type);
+}
+// API listing
+///////////////////////////////////////////////////////////////////
+router.post('/api/GetMember', async function(req, res, next)
+{
+    let scr = await getAllUserByType('member');
+    if (isManagerLoggingIn(req))
+    {
+        scr.forEach(user => {
+            user.password = '';
+        });
+        res.json(scr);
+    }            
+    else console.log('Unauthorize!');
+})
+router.post('/api/DeleteMember', async function(req, res, next)
+{
+    let scr = await getAllUserByType('member');
+    if (isManagerLoggingIn(req))
+    {
+        scr.forEach(user => {
+            user.password = '';
+        });
+        res.json(scr);
+    }            
+    else 
+    {
+        res.json(false);
+    }
+})
+///////////////////////////////////////////////////////////////////////////
 router.get('/', function(req, res, next) {
+    console.log(req);
     if (isManagerLoggingIn(req))
         res.redirect('/manager/dashboard');
     else {
@@ -41,7 +77,7 @@ router.post('/login', async function(req, res, next) {
 
 router.get('/dashboard', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Quản lí", h1: "Smart Food Court System", p: "Trang dành cho Quản lí", userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Quản lí" , plt: "none",userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
@@ -49,35 +85,45 @@ router.get('/dashboard', function(req, res, next) {
 
 router.get('/manage-screen', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Quản lí Screen", h1:"", p:"", userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Quản lí Screen", plt: 'screen', userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
 });
 router.get('/manage-vendor', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Quản lí Screen",  userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Quản lí nhà cung cấp", plt: 'vendor',  userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
 });
 router.get('/manage-cashier', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Quản lí Screen",  userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Quản lí thu ngân", plt:'cashier',  userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
 });
 router.get('/manage-cook', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Quản lí cook", userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Quản lí đầu bếp", plt:'cook', userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
 });
+router.get('/manage-member', async function(req, res, next) {
+    
+    if (isManagerLoggingIn(req)) {
+        
+        res.render('./manager/dashboard', { title: "Quản lí thành viên", plt: 'member', userType: req.session.type })
+    } else {
+        res.redirect('/manager/login');
+    }
+});
+
 router.get('/report', function(req, res, next) {
     if (isManagerLoggingIn(req)) {
-        res.render('./manager/dashboard', { title: "Lấy báo cáo", userType: req.session.type })
+        res.render('./manager/dashboard', { title: "Lấy báo cáo", plt: 'getReport', userType: req.session.type })
     } else {
         res.redirect('/manager/login');
     }
